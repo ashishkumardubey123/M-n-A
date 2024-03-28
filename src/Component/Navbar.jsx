6e4ce9c23d738eb372b18daa06ea4b5dc4c6ee6c
ashishkumardubey123 <ashish9039062705@gmail.com>
2024-03-29 01:25:20 +0530

    code clean

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className=' bg-slate-600 flex justify-center items-center  font-bold  text-white   uppercase    text-xl  h-[5rem] w-full '>
+      <p>  hello this is may nav bar  </p>
+    </div>
+  )
+}
+
+export default Navbar
